103220a4bd93402d1c776076f94222142ea4386f
belka-dev <belkacem.hadid@gmail.com>
2022-10-30 12:46:26 +0100

    Add Entity Framework and updates DataBase

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Hotel> Hotels { get; set; }   
+        public DbSet<Country> Counties { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country()
+                {
+                    Id = 1,
+                    Name   =  "Jamaica",
+                    ShortName="JM"
+                },
+                new Country()
+                {
+                    Id = 2,
+                    Name   =  "France",
+                    ShortName="FR"
+                },
+                new Country()
+                {
+                    Id = 3,
+                    Name   =  "United Kindom",
+                    ShortName="UK"
+                },
+                new Country()
+                {
+                    Id = 4,
+                    Name   =  "Itally",
+                    ShortName="IT"
+                },
+                new Country()
+                {
+                    Id = 5,
+                    Name   =  "Kabylie",
+                    ShortName="KB"
+                }
+                );
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel()
+                {
+                    Id = 1,
+                    Name="SAndals Hotel",
+                    CountryId=1,
+                    Rating=4.5
+                }, new Hotel()
+                {
+                    Id = 2,
+                    Name="Paris Hotel",
+                    CountryId=1,
+                    Rating=4.5
+                }, new Hotel()
+                {
+                    Id = 3,
+                    Name="UK Hotel",
+                    CountryId=3,
+                    Rating=4.5
+                }, new Hotel()
+                {
+                    Id = 4,
+                    Name="Itally Hotel",
+                    CountryId=4,
+                    Rating=4.5
+                }, 
+                new Hotel(){
+                    Id = 5,
+                    Name="Djurdjura Hotel",
+                    CountryId=5,
+                    Rating=4.5
+                }
+                ); ;
+        
+        }
+
+    }
+}
---
+++ HotelListing.API/Migrations/20221030114119_SeedContriesAndHotels.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class SeedContriesAndHotels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Counties",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Jamaica", "JM" },
+                    { 2, "France", "FR" },
+                    { 3, "United Kindom", "UK" },
+                    { 4, "Itally", "IT" },
+                    { 5, "Kabylie", "KB" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Adress", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, null, 1, "SAndals Hotel", 4.5 },
+                    { 2, null, 1, "Paris Hotel", 4.5 },
+                    { 3, null, 3, "UK Hotel", 4.5 },
+                    { 4, null, 4, "Itally Hotel", 4.5 },
+                    { 5, null, 5, "Djurdjura Hotel", 4.5 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Counties",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Counties",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Counties",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Counties",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Counties",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
